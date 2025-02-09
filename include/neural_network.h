12b6fcdbb5c903b45222a03298d16bf8939309e5
HugoKrona <hugo.krona@icloud.com>
2025-02-09 12:24:59 +0100

    Uppdated code remarks

--- include/neural_network.h
+++ include/neural_network.h
@@ -125,9 +125,7 @@ class NeuralNetwork {
      ******************************************************************************/
     void optimize(const std::vector<double> &output, const double learningRate);
 
-    /** @remark Please update the comment for myHiddenLayers below.
-     *          Consider writing something like "Hidden layers of the network".*/
-    std::vector<DenseLayer> myHiddenLayers;            // The network's single hidden layer.
+    std::vector<DenseLayer> myHiddenLayers;            // The network's hidden layers.
     DenseLayer myOutputLayer;                          // Output layer of the network.
     std::vector<std::vector<double>> myTrainingInput;  // Training input sets.
     std::vector<std::vector<double>> myTrainingOutput; // Training output sets.
--- source/main.cpp
+++ source/main.cpp
@@ -13,10 +13,6 @@
  *       extension, except makefiles, are ignored.
  */
 
-/** @remark Please update the comment below! */
-/********************************************************************************
- * @brief Example program demonstrating the Raspberry Pi GPIO drivers.
- ********************************************************************************/
 #include "button.h"
 #include "led.h"
 
@@ -25,7 +21,6 @@
 
 #include "neural_network.h"
 
-/** @note I added the latter part of this comment block. Please remove this comment. */
 /********************************************************************************
  * @brief Trains a neural network to learn the XOR function.
  * 
@@ -60,8 +55,8 @@ int main() {
         {1.0}, {0.0}, {1.0}, {1.0}, {0.0}, {1.0}, {0.0}, {0.0}, {1.0}, {0.0}, {1.0},
         {1.0}, {0.0}, {0.0}, {1.0}, {1.0}, {0.0}, {1.0}, {0.0}, {0.0}, {1.0}};
 
-    /** @remark Isn't this a 5-5x5-1 network, i.e. aren't you using five hidden layers? */
-    // Create a 5-5-5-1 neural network with hyperbolic tangent activation function.
+
+    // Create a 5-5x5-1 neural network with hyperbolic tangent activation function.
     ml::NeuralNetwork network{5U, 5U, 5U, 1U, ml::ActFunc::Tanh};
 
     // Add the training data.
@@ -71,13 +66,10 @@ int main() {
     if (network.train(epochCount, learningRate)) {
         network.printResults();
     }
-    // Else print an error message.
+    // Else print an error message and return.
     else {
         std::cout << "Failed to train the network!\n";
-
-        /** @remark Consider returning error code 1 here to terminate the program
-         *          if the training failed. In the current implementation, the system
-         *          will still work if the training of the network failed. */
+        return 1;
     }
 
     std::cout << "Training is done\n";
@@ -85,32 +77,17 @@ int main() {
     std::vector<double> input(5, 0.0);
 
     while (1) {
-        /** @remark This implementation is good. However, I can't help to mention that if you store
-         *          store the addresses of the buttons in a vector, you could replace the five lines
-         *          below with a for loop:
-         * 
-         *          std::vector<Button*> buttons{&button1, &button2, &button3, &button4, &button5};
-         *
-         *          for (std::size_t i{}; i < buttons.size(); ++i) { 
-         *              input[i] = buttons[i].isPressed() ? 1.0 : 0.0; 
-         *          }
-         * 
-         *          On the other hand, implementing my suggestion would require slightly more memory,
-         *          since we would have to add a vector holding five pointers. Also, even though
-         *          the for loop solution is more elegant, the difference will not be that big, 
-         *          since only five buttons are used. However, if something like 20 buttons were 
-         *          used, I would definitely suggest using the pointers. 
-         */
+        
         // Update input vector based on button states.
-        input[0] = button1.isPressed() ? 1.0 : 0.0;
-        input[1] = button2.isPressed() ? 1.0 : 0.0;
-        input[2] = button3.isPressed() ? 1.0 : 0.0;
-        input[3] = button4.isPressed() ? 1.0 : 0.0;
-        input[4] = button5.isPressed() ? 1.0 : 0.0;
+        std::vector<Button*> buttons{&button1, &button2, &button3, &button4, &button5};
+        
+        for (std::size_t i{}; i < buttons.size(); ++i) { 
+            input[i] = buttons[i].isPressed() ? 1.0 : 0.0; 
+        }
+
         
         // Predict output using the neural network.
-        /** @remark Consider using auto here, i.e. auto output{network.predict(input)}; */
-        std::vector<double> output = network.predict(input);
+        const auto output{network.predict(input)};
 
         // Enable LED based on the output.
         const auto enable{output[0] >= 0.5 ? true : false};
